58e8f23100c18c85e84ec992e7bd2b0646783fcf
MSoloz <atefmiladi08@gmail.com>
2023-08-29 00:41:37 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './components/signin/signin.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ClassesComponent } from './components/classes/classes.component';
+import { SubjectsComponent } from './components/subjects/subjects.component';
+import { ProgressComponent } from './components/progress/progress.component';
+import { RewardsComponent } from './components/rewards/rewards.component';
+import { QuestionsComponent } from './components/questions/questions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+ {
+   path:'',
+   component:SigninComponent
+ },
+ 
+ {path: 'ELearning', component:NavbarComponent,children: [
+  {path:'courses', component:ClassesComponent},
+  {path:'classes', component:ClassesComponent},
+  {path:'subjects', component:SubjectsComponent},
+  {path:'progress', component:ProgressComponent},
+  {path:'rewards', component:RewardsComponent},
+  {path:'questions', component:QuestionsComponent},
+ 
+]}
+ 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,63 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProgressComponent } from './components/progress/progress.component';
+import { RewardsComponent } from './components/rewards/rewards.component';
+import { ClassesComponent } from './components/classes/classes.component';
+import { SubjectsComponent } from './components/subjects/subjects.component';
+import { QuestionsComponent } from './components/questions/questions.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatIconModule } from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
+import {MatMenuModule} from '@angular/material/menu';
+import { CoursesComponent } from './components/courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProgressComponent,
+    RewardsComponent,
+    ClassesComponent,
+    SubjectsComponent,
+    QuestionsComponent,
+    SigninComponent,
+    SignupComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatToolbarModule,
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatListModule,
+    MatTableModule,
+    MatMenuModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
